ffd362ab8d1321b8e0a2778056d3fbc1f74e8914
Bibek Pandey <bibek.pandey@dishhome.com.np>
2025-12-31 11:05:06 +0545

    added request Validator

---
+++ Ecommerce.Core/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Core.Dto;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            // Email
+            RuleFor(temp => temp.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email address format");
+            // Password
+            RuleFor(temp => temp.Password)
+                .NotEmpty().WithMessage("Password is required");
+            // Validate the Person property
+            RuleFor(request => request.PersonName)
+                .NotEmpty().WithMessage("PersonName can't be blank")
+                .Length(1, 50).WithMessage("Person Name should be 1 to 50 characters long");
+            // Validate the Gender property
+            RuleFor(request => request.Gender)
+                .IsInEnum().WithMessage("Invalid gender option");
+        }
+    }
+}
